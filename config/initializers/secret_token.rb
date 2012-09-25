dc5bd0ea3429ef053c5d1052b1641711e5ec3c59
Stefan Vizzari <svizzari@zendesk.com>
2012-09-25 12:20:17 +1000

    Added rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scorer::Application.config.secret_token = '326f2f87e2c6cb6ddafe51ff5116ed57d62c8114242fccd5eb32770774feafba8c7adafa66f51e7708021fb7966578750851d0babf4e285132794ea7782cd99c'
