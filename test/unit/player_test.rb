eb08b3de92e7a1cc68a94e990da17296a323d796
Jason Smale <jsmale@zendesk.com>
2012-09-28 13:06:21 +1000

    Added fixtures

--- test/unit/player_test.rb
+++ test/unit/player_test.rb
@@ -4,4 +4,10 @@ class PlayerTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should not save a player without a first name" do
+    player = Player.new
+    assert !player.save
+  end
+
 end
