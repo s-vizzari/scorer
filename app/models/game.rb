f86ba9f01cf2e3571a92663a46f0fb7b60c1428f
Daniel Ribeiro <djbreves@gmail.com>
2013-01-17 00:43:42 +1100

    Updated game to set winner on save

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,13 @@
 class Game < ActiveRecord::Base
-  attr_accessible :score1, :score2, :match_id
+  attr_accessible :score1, :score2, :match_id, :winner
   belongs_to :match
+  belongs_to :winner, :class_name => "Player", :foreign_key => "winner_id"
   validates :score1, :presence => true
   validates :score2, :presence => true
   validates :match_id, :presence => true
+  before_save :set_winner 
+
+  def set_winner
+    self.winner = score1 > score2 ? match.p1 : match.p2
+  end
 end
